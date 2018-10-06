e16681456d6d5f56345d6ad264ad99a926ce5b08
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-10-05 22:29:55 -0400

    database and rout added

--- src/main/java/com/dkitaw/springapi/ui/controller/UserController.java
+++ src/main/java/com/dkitaw/springapi/ui/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.dkitaw.springapi.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,19 @@ public class UserController {
   public String getUser(){
     return "get user was called";
   }
-  public String creteUser(){
-    return "crete user was called";
+  @PostMapping
+  public String createUser(){
+    return "create user was called";
   }
+
+  @PutMapping
+  public String updateUser(){
+    return "update user was called";
+  }
+  @DeleteMapping
+  public String deleteUser(){
+    return "delete user was called";
+  }
+
+
 }
