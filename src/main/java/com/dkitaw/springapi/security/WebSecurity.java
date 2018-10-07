a06a8859c6a9ca083eaa2eac5523b3a3e19a32de
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-10-06 21:32:25 -0400

    Autentication filter added

--- src/main/java/com/dkitaw/springapi/security/WebSecurity.java
+++ src/main/java/com/dkitaw/springapi/security/WebSecurity.java
@@ -23,8 +23,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL) // `/user` should be public
-        .permitAll().anyRequest().authenticated(); // any other need to be authenticated
+    http.csrf().disable().authorizeRequests()
+        .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL) // `/user` should be public
+        .permitAll()
+        .anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager())); 
   }
 
   @Override
