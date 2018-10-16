70b53092d12f7b836c78346e688144c3d8857131
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-10-15 20:35:53 -0400

    proxy added

--- src/main/java/com/dkitaw/springapi/ui/controller/UserController.java
+++ src/main/java/com/dkitaw/springapi/ui/controller/UserController.java
@@ -7,6 +7,7 @@ import com.dkitaw.springapi.ui.model.response.UserRest;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class UserController {
 
   @Autowired
